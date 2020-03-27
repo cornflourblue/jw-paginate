76c92cbc2c39040198d176c815c12b60d241a968
Mohit karkar <46919688+mohit981@users.noreply.github.com>
2020-03-27 18:02:00 +0530

    Update jw-paginate.ts
    Improved little performance.
    Removed Array() because it is slow in this case 
    I have checked a benchmark from 3 sites
    
    In Array() benchmark was 707,662 ops/sec
    Adding for loop benchmark result was 4,674,123 ops/sec

--- src/jw-paginate.ts
+++ src/jw-paginate.ts
@@ -41,9 +41,15 @@ function paginate(
   // calculate start and end item indexes
   let startIndex = (currentPage - 1) * pageSize;
   let endIndex = Math.min(startIndex + pageSize - 1, totalItems - 1);
+  
+  let j = (endPage + 1) - startPage
+  let initArr = []
+    for (var i = 0; i < j; i++) {
+    initArr.push(i);
+  }
 
   // create an array of pages to ng-repeat in the pager control
-  let pages = Array.from(Array((endPage + 1) - startPage).keys()).map(i => startPage + i);
+  let pages = initArr.map(i => startPage + i);
 
   // return object with all pager properties required by the view
   return {
